3b62d938410b828713c45e853419eb824c47583b
Бартошук Константин Владимирович <konstantin.bartoshuk@ocrv.ru>
2017-06-02 11:58:16 +0300

    Fixed search with Ajax

--- AspNetMvcPlugins/AspNetMvcPlugins/Controllers/HomeController.cs
+++ AspNetMvcPlugins/AspNetMvcPlugins/Controllers/HomeController.cs
@@ -8,65 +8,38 @@ using Domain.Core;
 
 namespace AspNetMvcPlugins.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly IEnumerable<IFinder> _actions;
+	public class HomeController : Controller
+	{
+		private readonly IEnumerable<IFinder> _actions;
 
-        public HomeController(IEnumerable<IFinder> actions)
-        {
-            _actions = actions;
-        }
+		public HomeController(IEnumerable<IFinder> actions)
+		{
+			_actions = actions;
+		}
 
 		[HttpGet]
-        public ActionResult Index()
-        {
-            var modules = DependencyResolver.Current.GetServices<IPluginModule>();
-			ViewBag.Plugins = modules;
-
-			ISearchParameters searchParameters = SearchHelper.FillSearchParameters();
-            ViewBag.SearchedFiles = new List<string>();
-			return View(searchParameters);
-        }
-
-		[HttpPost]
-		[ActionName("Index")]
-		public ActionResult Find(SearchParameters parameters)
+		public ActionResult Index()
 		{
 			var modules = DependencyResolver.Current.GetServices<IPluginModule>();
 			ViewBag.Plugins = modules;
 
-            IFinder action = null;
-            if (!String.IsNullOrEmpty(parameters.PluginModuleId))
-                action = _actions.SingleOrDefault(m => m.FileExtension.Equals(parameters.PluginModuleId));
-            IEnumerable<string> searchedResult = SearchHelper.TestSearch(parameters, action);
-            ViewBag.SearchedFiles = searchedResult;
-
-            return PartialView(parameters);
+			ISearchParameters searchParameters = SearchHelper.FillSearchParameters();
+			ViewBag.SearchedFiles = new List<string>();
+			return View(searchParameters);
 		}
 
+		public PartialViewResult Find(SearchParameters parameters)
+		{
+			var modules = DependencyResolver.Current.GetServices<IPluginModule>();
+			ViewBag.Plugins = modules;
 
+			IFinder action = null;
+			if (!String.IsNullOrEmpty(parameters.PluginModuleId))
+				action = _actions.SingleOrDefault(m => m.FileExtension.Equals(parameters.PluginModuleId));
+			IEnumerable<string> searchedResult = SearchHelper.TestSearch(parameters, action);
 
+			return PartialView(searchedResult);
+		}
 
-
-
-
-
-
-
-
-
-        [HttpGet]
-        public ActionResult Finder()
-        {
-            ViewBag.Actions = _actions;
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Finder(String action)
-        {
-            ViewBag.Actions = _actions;
-            return View();
-        }
 	}
 }
