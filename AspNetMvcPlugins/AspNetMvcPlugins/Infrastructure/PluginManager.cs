0fb8fac0648d6ce7678c75113a6a1c3a0ccf1a6e
Бартошук Константин Владимирович <konstantin.bartoshuk@ocrv.ru>
2017-06-15 17:59:02 +0300

    Added simple PluginManager

---
+++ AspNetMvcPlugins/AspNetMvcPlugins/Infrastructure/PluginManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AspNetMvcPlugins.Infrastructure
+{
+	public class PluginManager
+	{
+		static PluginManager _manager;
+
+		public static PluginManager Manager
+		{
+			get { return _manager ?? new PluginManager(); }
+		}
+
+		internal IList<Assembly> Assemblies { get; set; }
+
+		public PluginManager()
+		{
+			Assemblies = new List<Assembly>();
+		}
+
+		public IList<Assembly> GetAssemblies()
+		{
+			return Assemblies;
+		}
+
+		public Assembly FindAssembly(String name)
+		{
+			return Assemblies.Where(x => x.GetName().Name.Equals(name)).FirstOrDefault();
+		}
+
+	}
+}
